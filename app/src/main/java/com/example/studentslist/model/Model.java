4553a0acb0af5c756d236fb04a74108de4a342bc
elad aviv <elad.aviv@workiz.com>
2022-12-15 22:57:37 +0200

    change studentList length to 15 students

--- app/src/main/java/com/example/studentslist/model/Model.java
+++ app/src/main/java/com/example/studentslist/model/Model.java
@@ -9,7 +9,7 @@ public class Model {
     public static final Model instance = new Model();
 
     private Model(){
-        for(int i=1;i<=20;i++){
+        for(int i=1;i<=15;i++){
             Student s = new Student("Student Dan",""+i, "052497558"+i,"colman street "+i, false, R.drawable.student_avatar);
             data.add(s);
         }
